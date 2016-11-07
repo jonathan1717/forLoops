f4b14c3ccb659e94339cbb98ff5088c476b881b4
jonathan.poister <jonathan.poister@MU-C1-08.mullen.pvt.k12.co.us>
2016-11-07 08:37:41 -0700

    final

---
+++ ForLoops/src/forLoops.java
@@ -0,0 +1,94 @@
+public class forLoops
+
+{
+
+	public static void main(String[] args)
+	{
+//			exercise1();
+//			exercise2();
+//			exercise3();
+//			exercise4();
+//			exercise5();
+	}
+	
+		public static void exercise1()
+		{
+			for (int i=0; i <5; i++ )
+				{
+					System.out.println(i+1);
+				}
+		}
+		
+		
+		public static void exercise2()
+		{
+			for(int i=0; i <11; i=i+2)
+				{
+					System.out.println(i+") I love Mullen!");
+				}
+		}
+		
+		public static void exercise3()
+		{
+			for (int i=0; i <5; i++)
+				{
+					System.out.print(i+1);
+				}
+		}
+		
+		
+		public static void exercise4()
+		{
+			for (int i=10; i>0; i=i-1)
+				{
+				System.out.println(i);
+				}		
+				
+				{
+					System.out.println("Lift off!");
+				}
+		}
+		
+		public static void exercise5()
+		{
+			for (int i=0; i<10; i++)
+				{
+					int coin=(int)(Math.random()*2)+1;
+					
+					
+					 
+					if (coin == 1)
+					
+					{
+						System.out.println("You flipped heads!");
+					}
+					
+					else if (coin == 2)
+					
+						{
+							System.out.println("You flipped tails!");
+						}
+					
+				}
+	
+			
+			
+			
+			
+		}
+	
+		
+
+}
+
+		
+	
+	
+	
+	
+	
+	
+	
+ 
+
+
